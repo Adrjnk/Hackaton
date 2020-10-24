f8189538f77998fa5231aa635912f465b1930c04
gusvald <61837927+gusvald@users.noreply.github.com>
2020-10-24 22:40:30 +0200

    Dodany User

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -2,14 +2,25 @@
 
 
 Logic::Logic()  {
-}
+    Szymonn.navX = 233;
+    Szymonn.Score = 666;
+    Szymonn.navY = 124;
 
-void Logic::setCoord(User &user) {
-    user.navX = 0;
-    user.navY = 0;
-}
+    Michu.navY = 123;
+    Michu.navY = 4352;
+    Michu.Score = 2341;
 
-void Logic::setScore(User &user) {
- //user.Score =+ user.getPoints;
+    Irena.Score =234;
+    Irena.navY = 4567;
+    Irena.navX = 34576;
+
+    Szymob.Driver_Name = "Szybki i wściekły";
+    Szymob.car = "Dniepr";
+    Szymob.navX = 234234;
+    Szymob.navY = 123;
+    Szymob.Score = 456456564;
+
+
+
 }
 
--- src/Logic.hpp
+++ src/Logic.hpp
@@ -1,17 +1,24 @@
 #ifndef DRIVEE_LOGIC_HPP
 #define DRIVEE_LOGIC_HPP
 #include "User.hpp"
+#include "Trip.hpp"
+#include "Driver.hpp"
 
 class Logic {
-    public:
-    void setCoord(User &user);
-    void setScore(User &user);
-
-
-private:
-    User user;
     Logic();
 
+private:
+    User Szymonn;
+    User Irena;
+    User Michu;
+
+    Driver Szymob;
+    Driver Ireeenka;
+    Driver Garzewawiak;
+
+    Trip jeden;
+    Trip dwa;
+    Trip trzy;
 };
 
 
