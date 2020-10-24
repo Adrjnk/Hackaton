1eaa61357c332c6334c348612b4fc8b791852c6b
gusvald <61837927+gusvald@users.noreply.github.com>
2020-10-24 21:49:44 +0200

    Dodany User

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -1,1 +1,15 @@
 #include "Logic.hpp"
+
+
+Logic::Logic()  {
+}
+
+void Logic::setCoord(User &user) {
+    user.navX = 0;
+    user.navY = 0;
+}
+
+void Logic::setScore(User &user) {
+    user.Score =+ user.getPoints;
+}
+
