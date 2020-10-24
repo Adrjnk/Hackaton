beb6528094b8d50fae299652904ccbc9820e3f3d
ireenkaaa <252979@student.pwr.edu.pl>
2020-10-24 22:41:34 +0200

    sfml main screen included

--- src/MainScreen.cpp
+++ src/MainScreen.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "MainScreen.hpp"
+
+MainScreen::MainScreen()
+{
+    buttonDriver.setTexture(resources.textureButtonDriver);
+    buttonPassenger.setTexture(resources.textureButtonPassenger);
+    appName.setTexture(resources.textureAppName);
+    buttonDriver.setPosition(16,817);
+    buttonPassenger.setPosition(278,818);
+    appName.setPosition(58,13);
+
+}
+
+void MainScreen::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    target.draw(appName);
+    target.draw(buttonDriver);
+    target.draw(buttonPassenger);
+
+}
+
+MainScreen::~MainScreen() {}
--- src/MainScreen.hpp
+++ src/MainScreen.hpp
@@ -4,9 +4,21 @@
 
 #ifndef DRIVEE_MAINSCREEN_HPP
 #define DRIVEE_MAINSCREEN_HPP
+#include <SFML/Graphics.hpp>
+#include "Resources.hpp"
 
+class MainScreen : public sf::Drawable{
+public:
+    MainScreen();
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-class MainScreen {
+    virtual ~MainScreen();
+
+private:
+    sf::Sprite buttonDriver;
+    sf::Sprite buttonPassenger;
+    sf::Sprite appName;
+    Resources resources;
 
 };
 
