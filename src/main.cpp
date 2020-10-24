4cc410da66c4582e43184032e18df079bbfeace5
Adrjnk <61837593+Adrjnk@users.noreply.github.com>
2020-10-24 20:08:08 +0200

    wzór

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(){
-std::string a;
-std::cout<<"siemanko";
-std::cin>>a;
-std::cout<<a;
+    srand( static_cast<unsigned int>(time(nullptr)));
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow win(sf::VideoMode(600, 1000), "Drivee",sf::Style::Default ,settings);
 
+    while (win.isOpen()) {
+        sf::Event event;
+        while (win.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                win.close();
+            }
+        }
+        win.display();
+        win.clear();
+    }
     return 0;
 }
